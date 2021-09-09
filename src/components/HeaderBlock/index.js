62610285bbd44156c0449fdb38c295dbd4830fcc
webmisfit <adletkoshanov@mail.ru>
2021-09-09 23:18:21 +0600

    Revert "pull-1"
    This reverts commit 11a1263c741dbb5e329c6f822d92e1f8187a6aef.

---
+++ src/components/HeaderBlock/index.js
@@ -0,0 +1,9 @@
+ import s from './style.module.css'
+//  import {ReactComponent as ReactLogo} from ''
+
+const HeaderBlock = (props) =>{
+  return <h1 className={s.h1}>Hello World</h1>
+}
+
+
+export default HeaderBlock
