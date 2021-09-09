39e4f71e9652fb3899e522b84deb08d8e299c671
webmisfit <adletkoshanov@mail.ru>
2021-09-09 23:09:58 +0600

    pull 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+
+
+import bg2 from './assets/bg2.jpg'
+import bg3 from './assets/bg3.jpg'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World
-        </p>
-        
-      </header>
-    </div>
-  );
+    <>
+      <Header title='Pokemon game' desc='Cool game'/>
+      <Layout title='Gnida' desc='No comments' urlBg={bg2}/>
+      <Layout title='Сникерс' desc='африканец' colorBg='red'/>
+      <Layout title='Иваныч' desc='люблю шалить' urlBg={bg3}/>
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
