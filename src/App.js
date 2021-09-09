73437451029df6aabef24d2d5500fd7bceb3c372
webmisfit <adletkoshanov@mail.ru>
2021-09-10 01:43:30 +0600

    Revert "homework-2"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-import Header from "./components/Header/Header";
-import Layout from "./components/Layout/Layout";
-import Footer from "./components/Footer/Footer";
-
-import bg2 from './assets/bg2.jpg'
-import bg3 from './assets/bg3.jpg'
-
-
-const App = () => {
+function App() {
   return (
-    <>
-      <Header title='Pokemon game' desc='Cool game'/>
-      <Layout title='Gnida' desc='No comments' urlBg={bg2}/>
-      <Layout title='Сникерс' desc='африканец' colorBg='red'/>
-      <Layout title='Иваныч' desc='люблю шалить' urlBg={bg3}/>
-      <Footer/>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, World
+        </p>
+        
+      </header>
+    </div>
+  );
 }
 
 export default App;
