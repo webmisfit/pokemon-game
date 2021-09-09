11a1263c741dbb5e329c6f822d92e1f8187a6aef
webmisfit <adletkoshanov@mail.ru>
2021-09-09 23:15:02 +0600

    pull-1

--- src/components/HeaderBlock/index.js
+++
@@ -1,9 +0,0 @@
- import s from './style.module.css'
-//  import {ReactComponent as ReactLogo} from ''
-
-const HeaderBlock = (props) =>{
-  return <h1 className={s.h1}>Hello World</h1>
-}
-
-
-export default HeaderBlock
